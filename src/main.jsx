776ff8e5159cff3ce6365cfe9f957a585cf7838c
Javisapi <javi.fernandez.castanon@gmail.com>
2026-06-06 16:36:23 +0200

    fix: bump cache version to force stats reset on mobile

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-const CACHE_VERSION = '2'
+const CACHE_VERSION = '3'
 if (sessionStorage.getItem('cache_version') !== CACHE_VERSION) {
   Object.keys(sessionStorage).forEach(k => {
     if (k.startsWith('player_')) sessionStorage.removeItem(k)
